dbe75a3fd9a76eb7a9edfaf5b951970a406148e7
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2017-12-29 00:14:42 -0200

    criando o mapa de rotas e definindo o roteamento

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router'; // trabalha com o pacote de rotas do angular
+
+// importa os componentes que as rotas vão apontar
+import { HomeComponent } from './home/home.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+
+// exporta uma constante de rotas do tipo Routes com objetos com path (url) e component (que sera renderizado)
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'restaurantes', component: RestaurantesComponent },
+    { path: 'diversao', component: DiversaoComponent },
+];
