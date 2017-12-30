772d452c55873fa5180e163d58e701f6c5d929f1
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2017-12-29 23:43:28 -0200

    ajustando o template restaurantes

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -10,13 +10,17 @@ import { OfertasService } from '../ofertas.service';
 })
 export class RestaurantesComponent implements OnInit {
 
+  public ofertas: Oferta[];
+
               // define qual atributo sera responsavel pelo servico sem precisar instanciar o servico
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() 
   {
     this.ofertasService.getOfertasPorCategoria('restaurante')
-        .then( (ofertas: Oferta[] ) => { console.log(ofertas) } )
+        .then( (ofertas: Oferta[] ) => { 
+          this.ofertas = ofertas; 
+        })
   }
 
 }
