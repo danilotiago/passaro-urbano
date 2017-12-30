1e47a4e27e6c1ef99a47e978fb755e156afd37f0
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2017-12-30 00:00:01 -0200

    criando e navegando para pagina ofertas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Routes } from '@angular/router'; // trabalha com o pacote de rotas do a
 import { HomeComponent } from './home/home.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
 
 // exporta uma constante de rotas do tipo Routes com objetos com path (url) e component (que sera renderizado)
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'restaurantes', component: RestaurantesComponent },
     { path: 'diversao', component: DiversaoComponent },
+    { path: 'oferta', component: OfertaComponent },
 ];
